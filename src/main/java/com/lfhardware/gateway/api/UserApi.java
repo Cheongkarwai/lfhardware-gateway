4c548ff7249a6b47d6a482d2b81a4ee00e8cd118
Cheong Kar Wai <cheongkarwai5@gmail.com>
2024-07-13 08:14:30 +0800

    change to log

--- src/main/java/com/lfhardware/gateway/api/UserApi.java
+++ src/main/java/com/lfhardware/gateway/api/UserApi.java
@@ -1,5 +1,6 @@
 package com.lfhardware.gateway.api;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.ReactiveSecurityContextHolder;
 import org.springframework.security.oauth2.core.oidc.OidcUserInfo;
@@ -10,13 +11,14 @@ import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/api/gateway")
+@Slf4j
 public class UserApi {
 
     @GetMapping("/user-info")
     public Mono<String> findUsername(@AuthenticationPrincipal AuthenticationPrincipal authenticationPrincipal){
 
         return ReactiveSecurityContextHolder.getContext().flatMap(context-> {
-            System.out.println(context.getAuthentication().getName());
+            log.info("Finding user information : {}", context.getAuthentication().getName());
             return Mono.just(context.getAuthentication().getName());
         });
     }
